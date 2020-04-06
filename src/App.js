620b9e6b02b733b62f0d2b1a71a92a3e84d4563d
AnastasiyaKolch <45988412+AnastasiyaKolch@users.noreply.github.com>
2020-04-06 18:51:31 +0500

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Stars from './components/Stars'
 import './App.css'
-import {films} from './components/data'
-import Film from './components/Film'
-
 
 export default function App() {
   return (
